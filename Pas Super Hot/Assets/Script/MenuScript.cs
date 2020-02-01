65a6e77246ee63d5b600c2002957104edbc8e6ab
Pythoncroquette <max.pro.wtf@outlook.fr>
2020-02-01 20:40:51 +0100

    Menu 50%

--- Pas Super Hot/Assets/Script/MenuScript.cs
+++ Pas Super Hot/Assets/Script/MenuScript.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public void Play()
+    {
+        SceneManager.LoadScene("PlayTestScene");
+    }
+
+    public void Quit()
+    {
+        // Build to run only
+        Application.Quit();
+    }
+
+   
+
+   
     void Start()
     {
         
