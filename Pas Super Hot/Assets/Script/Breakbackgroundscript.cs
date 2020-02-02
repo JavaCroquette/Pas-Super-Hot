a8fbc487c64ba059e12cb7170bac3adde97e6eef
Pythoncroquette <max.pro.wtf@outlook.fr>
2020-02-02 12:10:10 +0100

    Menu Fonctionnel 100%

---
+++ Pas Super Hot/Assets/Script/Breakbackgroundscript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Breakbackgroundscript : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    
+    
+
+    public void restart()
+    {
+        pausePanel.SetActive(false);
+        SceneManager.LoadScene("PlayTestScene");
+        Time.timeScale = 1;
+    }
+
+    public void quit()
+    {
+        // Build to run only
+        pausePanel.SetActive(false);
+        SceneManager.LoadScene("MenuScene");
+        Time.timeScale = 1;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Pas Super Hot/Assets/Script/KeybordMovement.cs
+++ Pas Super Hot/Assets/Script/KeybordMovement.cs
@@ -8,11 +8,15 @@ public class KeybordMovement : MonoBehaviour
 
     private float speed = 2000f;
     public Rigidbody body;
+    
     private float jumpstrenght = 1000f;
+
     void Start()
     {
         body = transform.GetComponent<Rigidbody>();
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -20,6 +24,9 @@ public class KeybordMovement : MonoBehaviour
         float backforward = Input.GetAxis("Horizontal");
         float leftright = Input.GetAxis("Vertical");
         float jump = Input.GetAxis("Jump");
+
+    
+        
         
         Vector3 move = (transform.right * backforward + transform.forward * leftright) * speed + transform.up * jump * jumpstrenght;
         body.velocity = move * Time.deltaTime;
